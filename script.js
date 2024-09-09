b6971be00b563d5335cad6e23444726d6c9c1460
Joshua <jaozomaro@ucdavis.edu>
2024-09-08 22:25:46 -0700

    Starting About Me page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const myNav = document.querySelector('.myNav');
 const mobileMenu = document.querySelector('#mobile');
 const menu = document.querySelector('.navMenu');
 
 mobileMenu.addEventListener('click', function() { 
+    myNav.classList.toggle('goDark');
     menu.classList.toggle('active');
     mobileMenu.classList.toggle('active');
 });
