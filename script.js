c45e5f46a40c7b256ce9432d54ac87f2649ae517
Joshua <jaozomaro@ucdavis.edu>
2024-09-11 20:17:56 -0700

    Finished Projects Page

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const myNav = document.querySelector('.myNav');
-const mobileMenu = document.querySelector('#mobile');
-const menu = document.querySelector('.navMenu');
+document.addEventListener("DOMContentLoaded", function() {
+    const projects = document.querySelectorAll(".projects figure");
+    let i = 0;
 
-mobileMenu.addEventListener('click', function() { 
-    myNav.classList.toggle('goDark');
-    menu.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
+    function showFirstProject() {
+        if(projects.length > 0) {
+            projects[0].classList.add("active");
+        }
+    }
+
+    showFirstProject();
+    const myNav = document.querySelector('.myNav');
+    const mobileMenu = document.querySelector('#mobile');
+    const menu = document.querySelector('.navMenu');
+
+    // Make sure the consts exist
+    if(myNav && mobileMenu && menu) {
+        mobileMenu.addEventListener('click', function() { 
+            myNav.classList.toggle('goDark');
+            menu.classList.toggle('active');
+            mobileMenu.classList.toggle('active');
+        });
+    }
+
+        function displayProject(i) {
+            console.log("WORKED!");
+            // Remove any displayed images
+            projects.forEach(x => {x.classList.remove("active");});
+        
+            // Add image only for the current i
+            projects[i].classList.add("active");
+        }
+        
+        window.otherProject = function() {
+            console.log("This is i right at click: "+i);
+            i++;
+
+            // if i is too large, reset to 0
+            if(i >= projects.length) {
+                i=0;
+            }
+            console.log("This is i after click: "+i);
+            displayProject(i);
+        }
 });
+
+
